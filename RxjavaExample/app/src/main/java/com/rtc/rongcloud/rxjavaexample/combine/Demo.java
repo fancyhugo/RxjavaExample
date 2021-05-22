8d3059628b1a7905787629df685e1502a3560fdb
fancyhugo <whhitwmq@163.com>
2021-05-22 18:27:07 +0800

    替换lambda表达式

--- RxjavaExample/app/src/main/java/com/rtc/rongcloud/rxjavaexample/combine/Demo.java
+++ RxjavaExample/app/src/main/java/com/rtc/rongcloud/rxjavaexample/combine/Demo.java
@@ -6,42 +6,56 @@ import java.util.ArrayList;
 import java.util.List;
 
 import io.reactivex.Observable;
+import io.reactivex.ObservableSource;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.functions.Function;
 import io.reactivex.schedulers.Schedulers;
 
 public class Demo {
     @SuppressLint("CheckResult")
     public static void main(String args[]) {
         NetWorkApi.getColumns()
-                .map(types -> {
-                    List<Observable<? extends List<? extends Item>>> requestObservableList = new ArrayList<>();
-                    for (String type : types) {
-                        switch (type) {
-                            case "a":
-                                requestObservableList.add(
-                                        NetWorkApi.getItemListOfTypeA().startWith(new ArrayList<ItemA>())
-                                );
-                                break;
-                            case "b":
-                                requestObservableList.add(
-                                        NetWorkApi.getItemListOfTypeB().startWith(new ArrayList<ItemB>())
-                                );
-                                break;
-                            case "c":
-                                requestObservableList.add(
-                                        NetWorkApi.getItemListOfTypeC().startWith(new ArrayList<ItemC>())
-                                );
-                                break;
+                .map(new Function<List<String>, List<Observable<? extends List<? extends Item>>>>() {
+                    @Override
+                    public List<Observable<? extends List<? extends Item>>> apply(@NonNull List<String> types) throws Exception {
+                        List<Observable<? extends List<? extends Item>>> requestObservableList = new ArrayList<>();
+                        for (String type : types) {
+                            switch (type) {
+                                case "a":
+                                    requestObservableList.add(
+                                            NetWorkApi.getItemListOfTypeA().startWith(new ArrayList<ItemA>())
+                                    );
+                                    break;
+                                case "b":
+                                    requestObservableList.add(
+                                            NetWorkApi.getItemListOfTypeB().startWith(new ArrayList<ItemB>())
+                                    );
+                                    break;
+                                case "c":
+                                    requestObservableList.add(
+                                            NetWorkApi.getItemListOfTypeC().startWith(new ArrayList<ItemC>())
+                                    );
+                                    break;
+                            }
                         }
+                        return requestObservableList;
                     }
-                    return requestObservableList;
                 })
-                .flatMap(requestObservables -> Observable.combineLatest(requestObservables, objects -> {
-                    List<Item> items = new ArrayList<>();
-                    for (Object response : objects) {
-                        items.addAll((List<? extends Item>) response);
+                .flatMap(new Function<List<Observable<? extends List<? extends Item>>>, ObservableSource<? extends List<Item>>>() {
+                    @Override
+                    public ObservableSource<? extends List<Item>> apply(@NonNull List<Observable<? extends List<? extends Item>>> requestObservables) throws Exception {
+                        return Observable.combineLatest(requestObservables, new Function<Object[], List<Item>>() {
+                            @Override
+                            public List<Item> apply(@NonNull Object[] objects) throws Exception {
+                                List<Item> items = new ArrayList<>();
+                                for (Object response : objects) {
+                                    items.addAll((List<? extends Item>) response);
+                                }
+                                return items;
+                            }
+                        });
                     }
-                    return items;
-                }))
+                })
                 .subscribeOn(Schedulers.io())
                 .observeOn(Schedulers.newThread())
                 .subscribe(data -> {
